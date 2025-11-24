b7110e8de14c08f288ae1b66c757780cc0c4f816
Jeuz Aebraham F. Buenaventura <jeuzbuenaventura.3@gmail.com>
2025-11-24 21:54:51 +0800

    Add files via upload

---
+++ activity22.py
@@ -0,0 +1,18 @@
+Jeuz = ['January','February','March','April','May','June','July','August','September','October','November','December']
+          
+print(Jeuz)
+
+for i in Jeuz:
+    print(f"Jeuz - {i}")
+
+Jeuz.reverse()
+print(Jeuz)
+
+
+fullname = 'Jeuz Buenaventura'
+new_list = list(fullname) 
+new_list.reverse()
+print(new_list)
+
+new_list.sort()
+print (new_list)
